f0fa34c55ad12fbc688d7750331d70b98a499ac1
GitHub Action <action@github.com>
2026-06-20 01:38:58 +0000

    Auto-update app version to 1.0.178 [skip ci]

--- config/app-version.js
+++ config/app-version.js
@@ -5,8 +5,8 @@
  */
 
 export const APP_CONFIG = {
-  version: '1.0.177',
-  buildTimestamp: '2026-06-18T22:24:45Z',
+  version: '1.0.178',
+  buildTimestamp: '2026-06-20T01:38:58Z',
   environment: 'production', // può essere 'development', 'staging', o 'production'
   updateCheckInterval: 60 * 60 * 1000, // 1 ora in millisecondi
   skipUpdateCheck: false, // impostare a true durante lo sviluppo locale
